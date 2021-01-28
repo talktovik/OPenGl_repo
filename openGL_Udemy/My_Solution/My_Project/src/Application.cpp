8bc97d2780ab8f8f93916bf439a445a8c91d06cc
Vikas Yadav <32956678+talktovik@users.noreply.github.com>
2021-01-28 07:03:56 +0530

    Shaders Added,Commit1

--- openGL_Udemy/My_Solution/My_Project/src/Application.cpp
+++ openGL_Udemy/My_Solution/My_Project/src/Application.cpp
@@ -16,13 +16,31 @@ const GLint WIDTH = 800, HEiGHT = 600;
 GLuint VAO, VBO, shader;
 
 //Vertex Shader
-static const char* vshader = "                  \n\
-#version 330                                     \n\
-layout(location = 0) in vec3 pos;                 \n\
-  void main (){                                    \n\
-    gl_position = vec4(pos.x, pos.y, pos.z ,1.0);   \n\
+// Version 330 is glsl version
+// vec3 holds 3 position where vec4 holds 4 values.
+static const char* vshader = "                    \n\
+#version 330                                       \n\
+layout(location = 0) in vec3 pos;                   \n\
+  void main (){                                     \n\
+    gl_Position = vec4(0.4 *pos.x, 0.4 * pos.y, pos.z ,1.0);   \n\
 ";
 
+//Fragment shader
+static const char* fshader = "                    \n\
+#version 330                                       \n\
+out vec4 colour ;                                   \n\
+  void main (){                                     \n\
+   colour = vec4(0.0,0.0,0.0,1.0);   \n\
+";
+
+
+void compileshader() {
+
+    shader = glCreateProgram();
+    printf("Error Creating Shader program");
+    return;
+
+}
 
 void CreateTriange() {
     GLfloat vertices[] = {
