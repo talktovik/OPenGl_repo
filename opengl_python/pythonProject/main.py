d051ff08b7103c8baf109fa33a1c0a9f6ca2601f
talktovikas <vikas.yadav@progress.com>
2024-08-07 01:28:10 +0530

    Test commit
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- opengl_python/pythonProject/main.py
+++ opengl_python/pythonProject/main.py
@@ -3,9 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+#this is a comment.
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
